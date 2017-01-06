4888c279adbabb3fd0cb70dd0c4ba641d6c5aa3e
lzmhlzsd <332847979@qq.com>
2017-01-06 16:57:09 +0800

    m

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # msg-center-push-beta
+
+connect：连接成功
+connecting：正在连接
+disconnect：断开连接
+connect_failed：连接失败
+error：错误发生，并且无法被其他事件类型所处理
+message：同服务器端message事件
+anything：同服务器端anything事件
+reconnect_failed：重连失败
+reconnect：成功重连
+reconnecting：正在重连
+在这里要提下客户端socket发起连接时的顺序。当第一次连接时，事件触发顺序为：connecting->connect；当失去连接时，事件触发顺序为：disconnect->reconnecting（可能进行多次）->connecting->reconnect->connect。
--- msg-center-push1/push-server.js
+++ msg-center-push1/push-server.js
@@ -12,39 +12,56 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 })
 
+app.get('/push', function(req, res){
 
+    var room = req.query.token;
+    var data = req.query.data;
+    console.log('push: ' + room);
+    console.log('push: ' + data);
+    io.sockets.in(room).emit('news', { port: port, handshake: data })
+    res.send('ok');
+})
 // io.sockets.on('connection', function(socket) {
 
-// 	console.log(socket)
+//  console.log(socket)
 //     //socket.emit('news', { port: port });
 //     // socket.on('my other event', function(data) {
 //     //     console.log(data);
 //     // });
 //     socket.on('disconnect', function() {
-//     	console.log('客户端断开连接.......')
+//      console.log('客户端断开连接.......')
 //         io.emit('user disconnected');
 //     });//
 // });
 
-io.set('authorization', function(handshakeData, callback){
-	console.log(handshakeData._query.token)
-	if(handshakeData._query.token == '123456'){
-		callback(null, true);
-	}
-	else{
-		callback('error', false);
-	}
+var tokens = [
+    '123', '124', '125', '126'
+]
+
+
+io.set('authorization', function(handshakeData, callback) {
+    console.log(handshakeData._query.token)
+    if (handshakeData._query.token == '123456' || handshakeData._query.token == '234567') {
+        callback(null, true);
+        
+    } else {
+        callback('error', false);
+    }
 })
 
 io.sockets.on('connection', function(socket) {
-    console.log('客户端连接成功')
-    console.log(socket.handshake.query.token);
+    console.log('客户端连接成功，issued：' + socket.handshake.issued + ', ip：' + socket.handshake.headers['x-forwarded-for'])
+    console.log('加入房间号：' + socket.handshake.query.token);
+    socket.join(socket.handshake.query.token);
     socket.emit('news', { port: port, handshake: socket.handshake });
     socket.on('disconnect', function() {
         console.log('客户端断开连接.......')
     });
+
+    setTimeout(function(){
+        socket.emit('news', { port: port, handshake: 123 });
+    }, 5000)
 });
-io.adapter(redis({ host: 'localhost', port: 6379 }));
 
 server.listen(port, function() {
     console.log('listening on *:' + port);
